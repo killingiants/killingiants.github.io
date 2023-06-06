2a52dd0fb12deb13b8877f1c38a2c15f88d177e6
charles9904 <charlottabade@gmail.com>
2023-06-06 18:08:25 +0000

    :construction: setup & configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: [
+    "@vueuse/nuxt",
+    "@nuxtjs/eslint-module",
+    "@nuxtjs/i18n",
+  ],
+
+  devtools: { enabled: true },
+
+  eslint: {
+    lintOnStart: false,
+  }
 })
